af2783e6adad8a2977de7ce4958c0fa359865761
NeoPhyte27 <aldijinkpro27@gmail.com>
2024-11-06 20:14:08 +0700

    menentukan nilai di pertanyaan 4

--- function-conditional.php
+++ function-conditional.php
@@ -74,12 +74,28 @@
         buatlah sebuah function bernama tentukan_nilai . Di dalam function tentukan_nilai yang menerima parameter berupa integer. dengan ketentuan jika paramater integer lebih besar dari sama dengan 85 dan lebih kecil sama dengan 100 maka akan mereturn String “Sangat Baik” Selain itu jika parameter integer lebih besar sama dengan 70 dan lebih kecil dari 85 maka akan mereturn string “Baik” selain itu jika parameter number lebih besar sama
         dengan 60 dan lebih kecil dari 70 maka akan mereturn string “Cukup” selain itu maka akan mereturn string “Kurang” */
         // Code function di sini
-        // Hapus komentar di bawah ini untuk jalankan code
-        // echo tentukan_nilai(98); //Sangat Baik
-        // echo tentukan_nilai(76); //Baik
-        // echo tentukan_nilai(67); //Cukup
-        // echo tentukan_nilai(43); //Kurang
-        
+        function tentukan_nilai($nilai){
+            if($nilai >= 85 && $nilai <= 100){
+                return "Sangat Baik";
+            } else if( $nilai >= 70 && $nilai <85){
+                return "Baik";
+            } else if ($nilai >= 60 && $nilai < 70){
+                return "Cukup";
+            } else {
+                return "Kurang";
+            }
+        }
+        echo "Nilai Anda = 98 ";
+        echo tentukan_nilai(98); //Sangat Baik
+        echo "<br>";
+        echo "Nilai Anda = 76 ";
+        echo tentukan_nilai(76); //Baik
+        echo "<br>";
+        echo "Nilai Anda = 67 ";
+        echo tentukan_nilai(67); //Cukup
+        echo "<br>";
+        echo "Nilai Anda = 43 ";
+        echo tentukan_nilai(43); //Kurang
         ?>
     </body>
 
